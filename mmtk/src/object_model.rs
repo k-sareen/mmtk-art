0e96cd4366be5ccb570ee4ec3ea431e558d8f9d6
Kunal Sareen <kunal.sareen@anu.edu.au>
2025-04-10 06:17:17 +0000

    Don't use lockword for forwarding pointer for `simple_scan_object`
    This is not required anymore since we've made ART aware of MMTk
    forwarding pointers so it can correctly follow them.

--- mmtk/src/object_model.rs
+++ mmtk/src/object_model.rs
@@ -9,12 +9,8 @@ pub struct ArtObjectModel;
 
 impl ObjectModel<Art> for ArtObjectModel {
     const GLOBAL_LOG_BIT_SPEC: VMGlobalLogBitSpec = VMGlobalLogBitSpec::side_first();
-    #[cfg(not(feature = "simple_scan_object"))]
     const LOCAL_FORWARDING_POINTER_SPEC: VMLocalForwardingPointerSpec =
         VMLocalForwardingPointerSpec::in_header(0);
-    #[cfg(feature = "simple_scan_object")]
-    const LOCAL_FORWARDING_POINTER_SPEC: VMLocalForwardingPointerSpec =
-        VMLocalForwardingPointerSpec::in_header(32);
     const LOCAL_FORWARDING_BITS_SPEC: VMLocalForwardingBitsSpec =
         VMLocalForwardingBitsSpec::in_header(0);
     const LOCAL_LOS_MARK_NURSERY_SPEC: VMLocalLOSMarkNurserySpec =
